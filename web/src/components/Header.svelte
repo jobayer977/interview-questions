b8a9d3cc81ebd8fa0dee1f0411beeb0e7046eb32
Jobayer Hossain <jobayerhossain977@Gmail.com>
2023-01-01 09:27:37 +0600

    main: Added

---
+++ web/src/components/Header.svelte
@@ -0,0 +1,55 @@
+<script>
+</script>
+
+<header class="header">
+	<div class="container">
+		<div class="header-inner">
+			<a href="/" class="logo">
+				<img
+					src="https://d24y9kuxp2d7l2.cloudfront.net/assets/icons/exercism-with-logo-black-b427c06c6a068ba9f391734115e4d22dfa876d1d.svg"
+					alt=""
+				/>
+			</a>
+			<nav class="navs">
+				<ul>
+					<li><a href="/">About</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+</header>
+
+<style>
+	.header-inner {
+		height: 64px;
+		background: #fff;
+		border-bottom: 1px solid #eaecf3;
+		display: flex;
+		align-items: center;
+	}
+	.header {
+		height: 64px;
+		background: #fff;
+		border-bottom: 1px solid #eaecf3;
+		display: flex;
+		align-items: center;
+	}
+	.navs {
+		flex: 1;
+		display: flex;
+		justify-content: flex-end;
+	}
+	.navs ul {
+		display: flex;
+	}
+	.navs ul li a {
+		align-items: center;
+		color: #5d5489;
+		display: flex;
+		font-size: 16px;
+		font-weight: 600;
+		position: relative;
+		white-space: nowrap;
+		padding: 8px;
+	}
+</style>
