71230caeef85b80242dd6fc8cfde579d3f4c09ad
Jobayer Hossain <jobayerhossain977@Gmail.com>
2023-01-01 12:14:20 +0600

    main: Added

--- web/src/routes/[slug]/+page.ts
+++ web/src/routes/[slug]/+page.ts
@@ -1,6 +1,7 @@
-export const ssr = true;
+export const prerender = false;
 /** @type {import('./$types').PageLoad} */
 export async function load({ params, url }) {
+	console.log('🚀 ~ file: +page.ts:4 ~ load ~ url', url);
 	const res = await fetch(`${url.origin}/topics.json`);
 	const data = await res.json();
 	return {
